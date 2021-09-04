dc976d28521138ba4891964a304e31c8e8dfacf9
vivek-cd <saurabhraj14499@gmail.com>
2021-09-04 21:21:26 +0530

    5th setting url to data fetch

--- src/shared/baseUrl.js
+++ src/shared/baseUrl.js
@@ -1,1 +1,2 @@
-export const baseUrl = 'http://localhost:3001/';
+//export const baseUrl = 'http://localhost:3001/';
+export const baseUrl = "https://github.com/vivek-cd/Restaurant_catalog_ReactJs/tree/master/json-server/public/images/";
